9e8aff6f62c1dd63d8fb438360a9d419f23926f6
Lien Yuette <lienyuet@ox-m3.msk.21-school.ru>
2023-08-05 15:22:14 +0300

    done render

--- src/main.c
+++ src/main.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROW 80
-#define COL 25
+#define ROW 25
+#define COL 80
+#define CELL '\xF0\x9F\x8F\xBE'
 
-void scanf_int(int *input, int *no_err);
-void get_dynamic_matrix(int ***matrix, int row, int col, int *no_err, int *is_alloc);
-void free_dynamic_matrix(int **matrix, int *is_alloc);
-void input(int **matrix, int row, int col, int *no_err);
+void get_dynamic_matrix(char ***matrix, int row, int col, int *no_err, int *is_alloc);
+void free_dynamic_matrix(char **matrix, int *is_alloc);
+void input(char **matrix, int row, int col, int *no_err);
+void render_screen(char **matrix, int row, int col, int *no_err);
 
 int main() {
     int no_err = 1;
     int is_alloc = 0;
-    int **matrix;
+    char **matrix;
     int row = ROW, col = COL;
 
     get_dynamic_matrix(&matrix, row, col, &no_err, &is_alloc);
     input(matrix, row, col, &no_err);
-
-
-
+    if (no_err) {
+        render_screen(matrix, row, col, &no_err);
+    } else {
+        printf("Данные хуйня. Поешь говна");
+    }
     free_dynamic_matrix(matrix, &is_alloc);
-
     return 0;
 }
 
-void scanf_int(int *input, int *no_err) {
+
+void get_dynamic_matrix(char ***matrix, int row, int col, int *const no_err, int *is_alloc) {
     if (*no_err) {
-        char temp = '\0';
-
-        if (scanf("%d%c", input, &temp) < 1) {
-            *no_err = 0;
-        }
-
-        if (temp != '\0' && temp != '\n' && temp != ' ') {
-            *no_err = 0;
-        }
-    }
-}
-
-void get_dynamic_matrix(int ***matrix, int row, int col, int *const no_err, int *is_alloc) {
-    if (*no_err) {
-        *matrix = malloc(row * col * sizeof(int) + row * sizeof(int *));
-        int *ptr = (int *)(*matrix + row);
+        *matrix = malloc(row * col * sizeof(char) + row * sizeof(char *));
+        char *ptr = (char *)(*matrix + row);
 
         for (int i = 0; i < row; i++) {
             (*matrix)[i] = ptr + col * i;
@@ -50,35 +39,42 @@ void get_dynamic_matrix(int ***matrix, int row, int col, int *const no_err, int 
     }
 }
 
-void free_dynamic_matrix(int **matrix, int *is_alloc) {
+void free_dynamic_matrix(char **matrix, int *is_alloc) {
     if (*is_alloc) {
         free(matrix);
         *is_alloc = 0;
     }
 }
 
-void input(int **matrix, int row, int col, int *no_err) {
+void input(char **matrix, int row, int col, int *no_err) {
     if (*no_err) {
+        char ch;
         for (int i = 0; i < row && (*no_err); i++) {
             for (int j = 0; j < col && (*no_err); j++) {
-                scanf_int(&matrix[i][j], no_err);
+                scanf("%c", &ch);
+                if (ch != '0' && ch != '1' && ch != '\n' && ch != '\0') {
+                    *no_err = 0;
+                }
+                if (*no_err) {
+                    if (ch == '1' || ch == '0') {
+                        matrix[i][j] = (ch == '1' ? ch : ' ');
+                    }
+                }
             }
         }
     }
 }
 
-// void render_screen(int **matrix, int row, int col, int *no_err) {
-//   int x, y;
-//   for (y = 0; y <= FIELD_VER; y++) {
-//     for (x = 0; x <= FIELD_HOR; x++) {
-//         if (matrix[x][y] == 1) {
-//             printf("o");
-//         } else if (matrix[x][y] == 0) {
-//             printf(" ");
-//         }
-//       }
-//     }
-//     printf("\n");
-//   }
-// }
-//
+void render_screen(char **matrix, int row, int col, int *no_err) {
+  int x, y;
+  for (y = 0; y < row; y++) {
+    for (x = 0; x < col; x++) {
+        if (matrix[y][x] == '1') {
+            printf("\xF0\x9F\x8D\x86");
+        } else {
+            printf("%c", matrix[y][x]);
+        }
+    }
+    printf("\n");
+  }
+}
